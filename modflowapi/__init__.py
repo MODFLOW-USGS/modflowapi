519b09f92d3cb7b1ab7b99cecc7a88b4fea2ec09
jlarsen-usgs <jlarsen@usgs.gov>
2022-11-15 15:35:08 -0800

    Begin updating setup.py/.cfg method and CI for autotesting

--- modflowapi/__init__.py
+++ modflowapi/__init__.py
@@ -1,6 +1,5 @@
 # imports
+from .version import __version__
 from modflowapi.modflowapi import ModflowApi
 from . import interface
 from .interface.runner import run_model, Callbacks
-
-__version__ = "0.0.1"
---
+++ modflowapi/version.py
@@ -0,0 +1,6 @@
+# version file for modflowapi
+
+major = 0
+minor = 0
+micro = 2
+__version__ = f"{major}.{minor}.{micro}"
