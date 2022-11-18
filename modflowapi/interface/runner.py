4ba8a2f35c056a014e3233393634684dcc1a2522
jlarsen-usgs <jlarsen@usgs.gov>
2022-11-18 10:47:48 -0800

    Cleanup test logic

--- modflowapi/interface/runner.py
+++ modflowapi/interface/runner.py
@@ -50,14 +50,6 @@ def run_model(dll, sim_path, callback, verbose=False, _develop=False):
         else:
             dll += ".dylib"
 
-    # sl = shutil.which(dll)
-    # if sl is None:
-    #     raise Exception(
-    #         f"The program {dll} does not exist or is not executable"
-    #     )
-    # else:
-    #     sl = pathlib.Path(dll).absolute()
-
     mf6 = ModflowApi(
         dll,
         working_directory=sim_path,
