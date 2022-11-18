b69c365c783782db7519c67e5da5698f65d0f4dc
jlarsen-usgs <jlarsen@usgs.gov>
2022-11-18 10:33:40 -0800

    add better logic for linux leading execute statement

--- modflowapi/interface/runner.py
+++ modflowapi/interface/runner.py
@@ -42,7 +42,11 @@ def run_model(dll, sim_path, callback, verbose=False, _develop=False):
         if platform.system().lower() == "windows":
             dll += ".dll"
         elif platform.system().lower() == "linux":
-            dll = "./" + dll + ".so"
+            if not dll.startswith("./"):
+                if not dll.startswith("/"):
+                    dll = "./" + dll + ".so"
+                else:
+                    dll = "." + dll + ".so"
         else:
             dll += ".dylib"
 
